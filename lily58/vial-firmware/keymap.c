44c86d5b9a19973ef60ad30499f85a91b84bf71e
Kyle Xiao <kylexiao20@gmail.com>
2023-08-05 23:26:27 -0400

    Add delete to lily58 keymap

--- lily58/vial-firmware/keymap.c
+++ lily58/vial-firmware/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_FN] = LAYOUT(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    _______, XXXXXXX, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX,                   TD(2),   TD(3),   TD(4),   KC_MINS, KC_EQL,  _______,
+    _______, XXXXXXX, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX,                   TD(2),   TD(3),   TD(4),   KC_MINS, KC_EQL,  KC_DEL,
     _______, XXXXXXX, KC_VOLD, XXXXXXX, XXXXXXX, XXXXXXX,                   TD(0),   KC_DOWN, KC_UP,   TD(1),   XXXXXXX, _______,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                          _______, _______, _______, _______, _______,  _______, _______, _______
